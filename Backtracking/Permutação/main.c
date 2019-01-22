187427129fdd18b4bf29dd366c62d5528bbb662f
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2019-01-22 03:13:19 -0300

    Ajustes na permutação

--- Backtracking/Permutação/main.c
+++ Backtracking/Permutação/main.c
@@ -2,11 +2,10 @@
 
 int main(int argc, char *argv[]){
 	int tam_recebido;
-  	printf("Digite o tamanho do vetor a ser permutado: ");
+  	printf("Digite o tamanho do vetor a ser permutado (o maximo eh 100): ");
   	scanf("%i", &tam_recebido);
- 	printf("O tamanho é %d\n", tam_recebido);
 
-	int v2[10];
+	int v[100]; //Tamanho do vetor maximo de numeros
 	int aux = 0;
 
 	while (aux < tam_recebido){
@@ -14,25 +13,11 @@ int main(int argc, char *argv[]){
 		printf("Digite um numero: ");
 	  	scanf("%i", &num);
 
-		v2[aux] = num;
+		v[aux] = num;
     	aux++;
   	}
 
-	int a = v2[2];
-
-	printf("%d\n", a);
-
-	// int tam_v = tam_recebido / sizeof(int);
-	// printf("O tamanho 2 é %d\n", tam_v);
-	//
-	//
-	// int v[] = {1, 2, 3, 4};
-	// int tam_v2 = sizeof(v) / sizeof(int);
-	// printf("O tamanho 3 é %d\n", tam_v2);
-
-
-	//
-	// permuta(v, 0, tam_v - 1);
+	permuta(v, 0, tam_recebido - 1); //Chama a funcao Backtracking para permutar
 
 	return 0;
 }
--- Backtracking/Permutação/permutacao.h
+++ Backtracking/Permutação/permutacao.h
@@ -9,41 +9,4 @@ void troca(int vetor[], int i, int j);
 
 void permuta(int vetor[], int inf, int sup);
 
-
-// #define maximoVertice 200
-//
-// typedef struct Aresta{
-//     int peso;
-//     int verticeOrigem;
-//     int verticeDestino;
-//     struct Aresta* prox;
-// }Aresta;
-//
-// typedef struct Grafo{
-//     int ponderado;
-//     int digrafo;
-//     int v[maximoVertice][maximoVertice];
-//     struct Aresta* arestas;
-// }Grafo;
-//
-// Grafo* inicializarGrafo(int ponderado, int digrafo); //ok
-//
-// Aresta* inserirAresta(Aresta* aresta, int verticeOrigem, int verticeDestino, int peso); //ok
-//
-// void inserirGrafo(Grafo* grafo, int verticeOrigem, int verticeDestino, int peso); //ok
-//
-// void ordenacao(Aresta* aresta); //ok
-//
-// void ordenacaoDoArquivo(Aresta* aresta); //ok
-//
-// void kruskal(Grafo* grafo, Grafo* grafo2); //ok
-//
-// int calcularPeso(Grafo *grafo); //ok
-//
-// void receberArquivo(char arquivo[], Grafo* grafo); //ok
-//
-// void escreverArquivo(FILE* arquivo, Grafo* grafo); //ok
-//
-// void imprimirGrafo(Grafo* grafo); //ok
-
 #endif //__PERMUTACAO_H__
